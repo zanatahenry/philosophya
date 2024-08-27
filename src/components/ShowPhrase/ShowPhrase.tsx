b9f3b340d1efb0c1ffcc34bce0b5e65dfac332ce
Henry Zanata <henryzanata@hotmail.com>
2024-08-27 00:37:06 -0300

    adjust: text

--- src/components/ShowPhrase/ShowPhrase.tsx
+++ src/components/ShowPhrase/ShowPhrase.tsx
@@ -1,8 +1,11 @@
 export default function ShowPhrase ({ author, phrase }: { author: string, phrase: string }) {
   return (
-    <div className="flex space-x-2 justify-center">
-      <h1 className="text-neutral-50">{phrase}</h1>
-      <h2 className="text-neutral-50">{author}</h2>
+    <div className="flex space-x-2 justify-center items-center">
+      <blockquote>
+        <p className="text-neutral-50 text-lg">{phrase}</p>
+      </blockquote>
+
+      <p className="text-neutral-50">-{author}</p>
     </div>
   )
 }
